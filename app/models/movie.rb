4115b147a4af4fb0f75edcf58dffb691835ed3a0
Ashlinn Brennan <asbren13@gmail.com>
2015-03-06 14:57:42 -0500

    added validations for movies and reviews

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,8 @@
 class Movie < ActiveRecord::Base
+  MPAA_RATING = %w{G PG PG-13 R NC-17}
+
   has_many :reviews, dependent: :destroy
+
+  validates :title, presence: true, uniqueness: true
+  validates :MPAA_rating, inclusion: {in: MPAA_RATING, message: "is Invalid"}
 end
