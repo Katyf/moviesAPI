67c68fdbdb69528cdcf516eda64d6990701afd49
Ashlinn Brennan <asbren13@gmail.com>
2015-03-08 20:11:51 -0400

    added more seeds to seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,6 +25,33 @@ Movie.create!(title: "X-Men 2",
         MPAA_rating: "PG-13",
         description: "More good super people fight bad regular people.")
 
+Movie.create!(title: "Frozen",
+        total_gross: 400736600,
+        release_date: "27-12-2013",
+        MPAA_rating: "PG",
+        description: "When the newly crowned Queen Elsa accidentally uses her power to turn things into ice to curse her home in infinite winter, her sister, Anna, teams up with a mountain man, his playful reindeer, and a snowman to change the weather condition.")
+
+Movie.create!(title: "The Wolf of Wall Street",
+        total_gross: 116866727,
+        release_date: "25-12-2013",
+        MPAA_rating: "R",
+        description: "Based on the true story of Jordan Belfort, from his rise to a wealthy stock-broker living the high life to his fall involving crime, corruption and the federal government.")
+Movie.create!(title: "Gravity",
+        total_gross: 274084951,
+        release_date: "04-10-2013",
+        MPAA_rating: "PG-13",
+        description: "A medical engineer and an astronaut work together to survive after a catastrophe destroys their shuttle and leaves them adrift in orbit. ")
+Movie.create!(title: "The Lego Movie",
+        total_gross: 274084951,
+        release_date: "07-02-2014",
+        MPAA_rating: "PG",
+        description: "An ordinary Lego construction worker, thought to be the prophesied 'Special', is recruited to join a quest to stop an evil tyrant from gluing the Lego universe into eternal stasis.")
+Movie.create!(title: "The Hunger Games",
+        total_gross: 274084951,
+        release_date: "23-03-2012",
+        MPAA_rating: "PG-13",
+        description: "Katniss Everdeen voluntarily takes her younger sister's place in the Hunger Games, a televised fight to the death in which two teenagers from each of the twelve Districts of Panem are chosen at random to compete.")
+
 Movie.last.reviews << Review.create!(author: 'Steven', body: 'It was good', rating: 4)
 Movie.last.reviews << Review.create!(author: 'Stephen', body: 'It was ok', rating: 3)
 
